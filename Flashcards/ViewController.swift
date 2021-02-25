fffd0e9da531840217eff1c66ebe561b8b33325d
Warith Balogun <warithb2001@gmail.com>
2021-02-25 14:49:18 -0500

    simple flash cards app done

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Warith Balogun on 2/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
+}
+
